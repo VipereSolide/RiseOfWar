c4876e263b111b9dce7472d279a5f641a80afb77
VipereSolide <68956220+VipereSolide@users.noreply.github.com>
2023-03-20 18:46:22 +0100

    Fixed capture point bug. (2)

--- src/Rise Of War/CapturePointPatcher.cs
+++ src/Rise Of War/CapturePointPatcher.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using System.Collections.Generic;
+using System;
 
 using UnityEngine;
 using HarmonyLib;
@@ -9,6 +10,32 @@ namespace RiseOfWar
 
     public class CapturePointPatcher
     {
+        static Actor[] ActorsInCapturePointRange(CapturePoint point)
+        {
+            List<Actor> _actors = ActorManager.AliveActors();
+            List<Actor> _output = new List<Actor>();
+
+            foreach(Actor _actor in _actors)
+            {
+                if (point.owner == _actor.team)
+                { 
+                    if (point.IsInsideProtectRange(_actor.CenterPosition()))
+                    {
+                        _output.Add(_actor);
+                    }
+                }
+                else
+                {
+                    if (point.IsInsideCaptureRange(_actor.CenterPosition()))
+                    {
+                        _output.Add(_actor);
+                    }
+                }
+            }
+
+            return _output.ToArray();
+        }
+
         [HarmonyPatch(typeof(CapturePoint), "SetOwner")]
         [HarmonyPrefix]
         static void SetOwnerPatch(CapturePoint __instance, int team, bool initialOwner = false)
@@ -21,29 +48,35 @@ namespace RiseOfWar
             bool _isNeutralized = team == -1;
 
             OnCapturePointInteractionEvent _interaction = new OnCapturePointInteractionEvent(OnCapturePointInteractionEvent.InteractionType.Lost, ActorManager.AliveActorsInRange(__instance.transform.position, __instance.captureRange), __instance.owner, team, __instance);
+            Actor[] _actorsInRange = ActorsInCapturePointRange(__instance);
+
+            foreach(Actor _actor in _actorsInRange)
+            {
+                Debug.Log("Actors in range: " + _actor.name);
+            }
 
             if (_isNeutralized && __instance.owner == GameManager.PlayerTeam())
             {
                 // Lost
-                _interaction = new OnCapturePointInteractionEvent(OnCapturePointInteractionEvent.InteractionType.Lost, ActorManager.AliveActorsInRange(__instance.transform.position, __instance.captureRange), __instance.owner, team, __instance);
+                _interaction = new OnCapturePointInteractionEvent(OnCapturePointInteractionEvent.InteractionType.Lost, _actorsInRange, __instance.owner, team, __instance);
             }
 
             if (_isNeutralized && __instance.owner != GameManager.PlayerTeam())
             {
                 // Neutralized
-                _interaction = new OnCapturePointInteractionEvent(OnCapturePointInteractionEvent.InteractionType.Neutralized, ActorManager.AliveActorsInRange(__instance.transform.position, __instance.captureRange), __instance.owner, team, __instance);
+                _interaction = new OnCapturePointInteractionEvent(OnCapturePointInteractionEvent.InteractionType.Neutralized, _actorsInRange, __instance.owner, team, __instance);
             }
 
             if (!_isNeutralized && team == GameManager.PlayerTeam())
             {
                 // Enemy captured
-                _interaction = new OnCapturePointInteractionEvent(OnCapturePointInteractionEvent.InteractionType.Captured, ActorManager.AliveActorsInRange(__instance.transform.position, __instance.captureRange), __instance.owner, team, __instance);
+                _interaction = new OnCapturePointInteractionEvent(OnCapturePointInteractionEvent.InteractionType.Captured, _actorsInRange, __instance.owner, team, __instance);
             }
 
             if (!_isNeutralized && team != GameManager.PlayerTeam())
             {
                 // Player captured
-                _interaction = new OnCapturePointInteractionEvent(OnCapturePointInteractionEvent.InteractionType.EnemyCapture, ActorManager.AliveActorsInRange(__instance.transform.position, __instance.captureRange), __instance.owner, team, __instance);
+                _interaction = new OnCapturePointInteractionEvent(OnCapturePointInteractionEvent.InteractionType.EnemyCapture, _actorsInRange, __instance.owner, team, __instance);
             }
 
             EventManager.onCapturePointInteraction.Invoke(_interaction);
