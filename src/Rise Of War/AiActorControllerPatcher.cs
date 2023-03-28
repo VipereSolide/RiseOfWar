8d0edb9f90bccfafc9f65750677d12c0373be33e
VipereSolide <68956220+VipereSolide@users.noreply.github.com>
2023-03-28 16:40:58 +0200

    Fixed squad issues with actor vehicle behaviour

--- src/Rise Of War/AiActorControllerPatcher.cs
+++ src/Rise Of War/AiActorControllerPatcher.cs
@@ -13,7 +13,7 @@ namespace RiseOfWar
 
         [HarmonyPatch(typeof(AiActorController), "EnterSeat")]
         [HarmonyPrefix]
-        private static bool EnterSeatPatch(AiActorController __instance, Seat seat)
+        private static bool EnterSeatPatch(AiActorController __instance, ref Seat seat)
         {
             Vehicle _vehicle = seat.vehicle;
 
@@ -26,6 +26,38 @@ namespace RiseOfWar
             if (_data.owner == null || (!_data.owner.dead && _vehicle.Driver() == _data.owner))
             {
                 return true;
+            }
+
+            seat = null;
+            return true;
+        }
+
+        [HarmonyPatch(typeof(AiActorController), "HasTargetVehicle")]
+        [HarmonyPrefix]
+        public static bool HasTargetVehiclePatch(AiActorController __instance, ref bool __result)
+        {
+            __result = false;
+
+            if (__instance.targetVehicle)
+            {
+                __result = true;
+
+                VehicleAdditionalData _data = __instance.targetVehicle.GetAdditionalData();
+
+                if (_data.owner != null && _data.owner.IsDriver() == false)
+                {
+                    __result = false;
+                }
+
+                if (_data.owner != null && __instance.targetVehicle.Driver() != _data.owner)
+                {
+                    __result = false;
+                }
+
+                if (_data.owner != null && _data.owner.dead)
+                {
+                    __result = false;
+                }
             }
 
             return false;
--- src/Rise Of War/SquadPatcher.cs
+++ src/Rise Of War/SquadPatcher.cs
@@ -12,26 +12,31 @@ namespace RiseOfWar
         [HarmonyPrefix]
         private static void DropMemberPatch(Squad __instance, ActorController a)
         {
-            if (a == __instance.Leader() && a.actor.IsSeated() && a.actor.IsDriver())
+            if (a == __instance.Leader())
             {
                 foreach (ActorController _actor in __instance.members)
                 {
                     if (_actor.actor.IsSeated() && _actor.actor.aiControlled)
                     {
-                        InitializeVehicleDrop(_actor);
+                        InitializeVehicleDrop(_actor, __instance);
                     }
                 }
             }
         }
 
-        private static void InitializeVehicleDrop(ActorController actorController)
+        private static void InitializeVehicleDrop(ActorController actorController, Squad squad)
         {
-            actorController.StartCoroutine(InternalInitVehicleDrop(actorController));
+            actorController.StartCoroutine(InternalInitVehicleDrop(actorController, squad));
         }
 
-        private static IEnumerator InternalInitVehicleDrop(ActorController actorController)
+        private static IEnumerator InternalInitVehicleDrop(ActorController actorController, Squad squad)
         {
             yield return new WaitForSeconds(0.75f + Random.Range(0f, 0.5f));
+
+            if (actorController.GetSquad() == squad)
+            {
+                squad.DropMember(actorController);
+            }
 
             actorController.actor.LeaveSeat(false);
         }
